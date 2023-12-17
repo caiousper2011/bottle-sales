11595b85ad829c182261741e48a1637320557207
Caio Barbosa de Almeida <caiousper2011@hotmail.com>
2023-12-17 15:06:16 -0300

    test(Button): setting up environment for test with storybook

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { Primary } from './Button.stories'
+import { Primary, WithAsChild } from './Button.stories'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
@@ -14,5 +14,11 @@ describe('Button', () => {
     expect(button).toBeEnabled()
     expect(button).toHaveTextContent('Primary Button')
     expect(onClickMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('shoul render a tag', async () => {
+    render(<WithAsChild {...WithAsChild.args} />)
+
+    expect(screen.getByRole('link')).toBeInTheDocument()
   })
 })
